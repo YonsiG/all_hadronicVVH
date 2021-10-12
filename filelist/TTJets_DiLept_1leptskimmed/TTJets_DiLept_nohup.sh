922208555d05ff468b3c1d5ebb35394759962ff5
yonsi <GUYANXI@mail.ustc.edu.cn>
2021-10-12 12:24:10 -0700

    new filelists

--- filelist/TTJets_DiLept_1leptskimmed/TTJets_DiLept_nohup.sh
+++
@@ -1,2 +0,0 @@
-nohup ./efficiency.exe filelist/TTJets_SingleLeptFromT_1lepskimmed/TTJets_DiLept0.txt TTJets_DiLept 0 &
-nohup ./efficiency.exe filelist/TTJets_SingleLeptFromT_1lepskimmed/TTJets_DiLept1.txt TTJets_DiLept 1 &
